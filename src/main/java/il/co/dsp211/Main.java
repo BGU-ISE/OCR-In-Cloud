f6e9cc6641967c27b176cbc67a9516e7b464e726
Roy Ash <roy.ash456@gmail.com>
2020-11-21 22:56:21 +0200

    user data working!!!

--- src/main/java/il/co/dsp211/Main.java
+++ src/main/java/il/co/dsp211/Main.java
@@ -7,9 +7,7 @@ import software.amazon.awssdk.services.ec2.model.InstanceType;
 import software.amazon.awssdk.services.ec2.model.RunInstancesRequest;
 import software.amazon.awssdk.services.ec2.model.RunInstancesResponse;
 
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class Main
@@ -22,15 +20,18 @@ public class Main
 
 		RunInstancesResponse response = ec2.runInstances(RunInstancesRequest.builder()
 				.instanceType(InstanceType.T2_MICRO)
-				.imageId("ami-00acfbfd2e91ae1b0")
+				.imageId("ami-00acfbfd2e91ae1b0") // Ubuntu Server 20.04 LTS (HVM), SSD Volume Type
+//				.imageId("ami-076515f20540e6e0b") // requested by assignment 1 but not working
+//				.imageId("ami-04bf6dcdc9ab498ca") // Amazon Linux 2 AMI (HVM), SSD Volume Type
 				.maxCount(1)
 				.minCount(1)
 				.keyName("RoysKey")
 				.securityGroupIds("sg-0210d89a3003c1298")
-				.userData(Base64.getEncoder().encodeToString("echo hello world > hello_world.txt\n".getBytes()))
+				.userData(Base64.getEncoder().encodeToString("""
+				                                             #!/bin/sh
+				                                             echo hello world > /home/ubuntu/hello_world.txt""".getBytes()))
 				.build());
 
-//		String instanceId = response.instances().get(0).instanceId();
 		System.out.println(response.instances().stream()
 				                   .map(Instance::instanceId)
 				                   .collect(Collectors.toList()) + " created!\nBye Bye");
