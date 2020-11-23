0eaf260e0678c6aa2c14b8213c18126874a3cb39
Roy Ash <roy.ash456@gmail.com>
2020-11-23 15:32:10 +0200

    temp

--- src/main/java/il/co/dsp211/Main.java
+++ src/main/java/il/co/dsp211/Main.java
@@ -2,16 +2,28 @@ package il.co.dsp211;
 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.ec2.model.Instance;
-import software.amazon.awssdk.services.ec2.model.InstanceType;
-import software.amazon.awssdk.services.ec2.model.RunInstancesRequest;
-import software.amazon.awssdk.services.ec2.model.RunInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.*;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.sqs.SqsClient;
 
 import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main
 {
+	private final static S3Client s3 = S3Client.builder()
+			.region(Region.US_EAST_1)
+			.build();
+		String queue_name = "queue" + System.currentTimeMillis();
+		SqsClient sqsClient = SqsClient.builder()
+			.region(Region.US_WEST_2)
+			.bEAST_1;
+
 	public static void main(String[] args)
 	{
 		Ec2Client ec2 = Ec2Client.builder()
@@ -32,8 +44,53 @@ public class Main
 				                                             echo hello world > /home/ubuntu/hello_world.txt""".getBytes()))
 				.build());
 
-		System.out.println(response.instances().stream()
-				                   .map(Instance::instanceId)
-				                   .collect(Collectors.toList()) + " created!\nBye Bye");
+		System.out.println(printInstancesState(ec2, response));
+
+		ec2.waiter().waitUntilInstanceRunning(DescribeInstancesRequest.builder()
+				.instanceIds(response.instances().stream()
+						.map(Instance::instanceId)
+						.toArray(String[]::new))
+				.build())
+				.matched()
+				.exception()
+				.ifPresent(System.out::println);
+
+		System.out.println(printInstancesState(ec2, response) + "\nBye Bye");
+
+
+//		String bucket = "bucket" + System.currentTimeMillis();
+//		createBucket(bucket, Region.US_EAST_1);
+	}
+
+	private static Map<String, List<InstanceStateName>> printInstancesState(Ec2Client ec2, RunInstancesResponse response)
+	{
+		return ec2.describeInstances(DescribeInstancesRequest.builder()
+				.instanceIds(response.instances().stream()
+						.map(Instance::instanceId)
+						.toArray(String[]::new))
+				.build())
+				.reservations().stream()
+				.flatMap(reservation -> reservation.instances().stream())
+				.collect(Collectors.groupingBy(Instance::instanceId, Collectors.mapping(instance -> instance.state().name(), Collectors.toList())));
+	}
+
+	private static void createBucket(String bucketName, Region region)
+	{
+		s3.createBucket(CreateBucketRequest.builder()
+				.bucket(bucketName)
+				.createBucketConfiguration(CreateBucketConfiguration.builder()
+						.locationConstraint(region.id())
+						.build())
+				.build());
+
+		s3.waiter().waitUntilBucketExists(HeadBucketRequest.builder()
+				.bucket(bucketName)
+				.build())
+				.matched()
+				.exception()
+				.ifPresent(System.out::println);
+		System.out.println(bucketName + " is ready");
+
+		System.out.println(bucketName);
 	}
 }
